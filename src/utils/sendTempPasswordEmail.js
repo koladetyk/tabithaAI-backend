384289b4640be20f0076d31cf378860558085752
koladetyk <ayokolade.erinle@gmail.com>
2025-07-04 18:40:12 -0400

    Sending password correctly

--- src/utils/sendTempPasswordEmail.js
+++ src/utils/sendTempPasswordEmail.js
@@ -4,7 +4,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 module.exports = async function sendTempPasswordEmail(email, tempPassword) {
   const msg = {
     to: email,
-    from: process.env.DEFAULT_EMAIL_FROM || 'no-reply@yourdomain.com',
+    from: process.env.FROM_EMAIL,
     subject: 'Your Temporary Login Password',
     text: `Your temporary password is: ${tempPassword}. Please log in and change it immediately.`,
   };
